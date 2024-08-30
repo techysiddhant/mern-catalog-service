dd2f7ba6917cd70e1cc05c0edac476ad04e2b969
techysiddhant <sidjain8000@gmail.com>
2024-08-30 17:13:11 +0530

    create product validation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import categoryRouter from "./category/category-router";
+import productRouter from "./product/product-router";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -9,6 +10,7 @@ app.get("/", (req: Request, res: Response) => {
     res.json({ message: "hello from catalog service" });
 });
 app.use("/categories", categoryRouter);
+app.use("/products", productRouter);
 app.use(globalErrorHandler);
 
 export default app;
---
+++ src/product/product-router.ts
@@ -0,0 +1,19 @@
+import express from "express";
+// import logger from "../config/logger";
+import { asyncWrapper } from "../common/utils/wrapper";
+import authenticate from "../common/middlewares/authenticate";
+import { canAccess } from "../common/middlewares/canAccess";
+import { Roles } from "../common/constants";
+import { ProductController } from "./product-controller";
+import createProductValidator from "./create-product-validator";
+const router = express.Router();
+const productController = new ProductController();
+router.post(
+    "/",
+    authenticate,
+    canAccess([Roles.ADMIN, Roles.MANAGER]),
+    createProductValidator,
+    asyncWrapper(productController.create.bind(productController)),
+);
+
+export default router;
