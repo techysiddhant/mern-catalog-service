4c0f59805b5027e255eb78bc1accfe46203560a8
techysiddhant <sidjain8000@gmail.com>
2024-09-02 13:39:12 +0530

    create product

--- src/product/product-controller.ts
+++ src/product/product-controller.ts
@@ -1,14 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NextFunction, Response } from "express";
 import { Request } from "express-jwt";
 import { validationResult } from "express-validator";
 import createHttpError from "http-errors";
+import { ProductService } from "./product-service";
+import { Product } from "./product-types";
 
 export class ProductController {
-    constructor() {}
+    constructor(private productService: ProductService) {}
     async create(req: Request, res: Response, next: NextFunction) {
         const result = validationResult(req);
         if (!result.isEmpty()) {
             return next(createHttpError(400, result.array()[0].msg as string));
         }
+        const {
+            name,
+            description,
+            priceConfiguration,
+            attributes,
+            tenantId,
+            categoryId,
+            isPublish,
+        } = req.body as Product;
+        const product = {
+            name,
+            description,
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            priceConfiguration: JSON.parse(priceConfiguration),
+            attributes: JSON.parse(attributes as unknown as string),
+            tenantId,
+            categoryId,
+            isPublish,
+            //TODO:image upload
+            image: "image.jpg",
+        };
+        const newProduct = await this.productService.createProduct(product);
+        res.json({ id: newProduct._id });
     }
 }
