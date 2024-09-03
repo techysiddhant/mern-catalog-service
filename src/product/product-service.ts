2e63ecb6a0f6a8a76796a09c8b2cf72abb4309e7
techysiddhant <sidjain8000@gmail.com>
2024-09-03 16:59:56 +0530

    remove extra db call

--- src/product/product-service.ts
+++ src/product/product-service.ts
@@ -5,10 +5,6 @@ export class ProductService {
     async createProduct(product: Product) {
         return await productModel.create(product);
     }
-    async getProductImage(productId: string) {
-        const product = await productModel.findById(productId);
-        return product?.image;
-    }
     async updateProduct(productId: string, product: Product) {
         return await productModel.findOneAndUpdate(
             {
